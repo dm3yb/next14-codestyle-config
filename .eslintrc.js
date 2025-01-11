5c7b3d3ee5634866a7091cb63aa8f3cd0bc2dd4d
Dmitry Bobryshev <dmitry.b09@gmail.com>
2025-01-11 18:41:31 +0100

    fix: update eslint configuration and package dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,29 @@
 /** @type { import("eslint").Linter.Config[] } */
 module.exports = {
-  extends: ['next/core-web-vitals', 'next', 'prettier'],
-  plugins: ['@next/next', 'unicorn'],
+  extends: [
+    'next',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+  ],
+  plugins: ['@next/next', 'unicorn', 'react-hooks', 'react'],
   rules: {
     '@next/next/no-img-element': 'warn',
     '@next/next/no-sync-scripts': 'error',
     '@next/next/no-html-link-for-pages': 'error',
     '@next/next/no-page-custom-font': 'warn',
+    'import/no-unresolved': ['error', { commonjs: true, amd: true }],
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-key': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'no-unused-vars': [
       'error',
       {
